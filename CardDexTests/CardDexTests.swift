78a8d6dd6d4633830084f29887c43956b50bbd79
Martin Plut <martinplutphoto@gmail.com>
2026-02-01 17:06:58 -0800

    Initial Commit

---
+++ CardDexTests/CardDexTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardDexTests.swift
+//  CardDexTests
+//
+//  Created by Martin Plut on 2/1/26.
+//
+
+import Testing
+@testable import CardDex
+
+struct CardDexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
